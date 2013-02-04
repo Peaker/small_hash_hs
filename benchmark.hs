956f57ac5319a7f09165c9cc962400368de7be83
Eyal Lotem <eyal.lotem@gmail.com>
2013-02-04 02:57:45 +0200

    Cmdline args in benchmark

--- benchmark.hs
+++ benchmark.hs
@@ -1,11 +1,17 @@
 {-# LANGUAGE BangPatterns #-}
 import Control.Monad
 import Data.SmallHash
+import System.Environment
 
 main :: IO ()
 main = do
+  args <- getArgs
+  num <-
+    case fmap read args of
+    [num] -> return num
+    _ -> fail "Usage: benchmark <count>"
   tbl <- new
-  forM_ [1..70000] $ \ !i -> do
+  forM_ [1..num] $ \ !i -> do
     insert tbl i i
   putStrLn "Done inserting"
   print =<< find tbl 100
